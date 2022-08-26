78c457a7b7420366d93e462cd0869447c66ee16e
LEKtestQ@gmail.com <LEKtestQ@gmail.com>
2022-08-26 18:56:25 +0300

    changed names

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int Refill = 200;
-        int Account = 100;
-        if(Refill >= 1000){
-            System.out.println("Количество бонусов равно " + (Refill / 100));
-            System.out.println("Состояние счета " + (Refill + Refill / 100));
-        }
-        else{
+        int refill = 200;
+        int account = 100;
+        int bonus;
+        if (refill > 1000) {
+            bonus = refill / 100;
+            System.out.println("Количество бонусов равно " + (bonus));
+            System.out.println("Состояние счета " + ((refill) + (account) + (bonus)));
+        } else {
             System.out.println("Количество бонусов равно 0");
-            System.out.println("Состояние счета " + (Refill + Account));
+            System.out.println("Состояние счета " + (refill + account));
         }
 
     }
